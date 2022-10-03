cdb0f272c8856a4e191b7e251d72c9f7537dc7cb
XDbitionist <eugene.sakharov@gmail.com>
2022-10-03 22:10:24 +0800

    Finished Task 8

---
+++ Task8(optional)/Program.cs
@@ -0,0 +1,17 @@
+﻿/* Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+5 -> 2, 4
+8 -> 2, 4, 6, 8 */
+Console.Clear();
+Console.WriteLine("Введите число, чтобы показать все четные числа до заданного числа: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int index = 1;
+Console.WriteLine("Четные: ");
+if (index == 1)
+{
+    index++;
+}
+while (index <= number)
+{
+    Console.WriteLine(index);
+    index += 2;
+}
